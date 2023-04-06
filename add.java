d1083de9c0afa593689f43731df3260569fabc02
devesh mishra <E22CSEU0766@bennett.edu.in>
2023-04-06 23:04:49 +0530

    add.java updated

---
+++ add.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class add {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter two numbers");
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        System.out.println("Your Sum is "+n1+n2);
+    }
+}
